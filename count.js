ee254c55e7f82121415db583f44bc6a5a9757e49
Taiwo-cloud-dotcom <taiwoadebowale24@yahoo.com>
2024-11-12 14:49:35 +1100

    first commit

--- count.js
+++ count.js
@@ -1,6 +1,7 @@
 let myTimeInSec = 7200
 
 let countdownDisplay = document.getElementById("counting")
+let stopdisplay = document .getElementById("stopco")
 
 function counting() {
     myTimeInSec -= 1;
@@ -14,6 +15,10 @@ function counting() {
     let myodu = document.getElementById('odu')
     let myuni = document.getElementById('uni')
 
+    hrs<10 ? hrs = `0${hrs}` : hrs
+    mins<10 ? mins = `0${mins}` : mins
+    sec<10 ? sec = `0${sec}` : sec
+
     mywa.innerText = hrs;
     myodu.innerText = mins;
     myuni.innerText = sec;
@@ -21,8 +26,12 @@ function counting() {
 }
 
 function lazz() {
-    setInterval(() => {
+    let countdownDisplay = setInterval(() => {
         counting()
 
-    }, 1000);
+    }, 100);
+
+    stopdisplay.addEventListener("click",() =>{
+        clearInterval(countdownDisplay)
+    })
 }
